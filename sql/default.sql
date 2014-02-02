9e31c2ce45a721b553c2e5a0851106d9e7e9e72b
vy007vikas <wicked.yadav@gmail.com>
2014-02-02 23:42:39 +0530

    Updated the default sql schema file

--- sql/default.sql
+++ sql/default.sql
@@ -3,8 +3,8 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 31, 2014 at 05:22 PM
--- Server version: 5.5.35-0ubuntu0.13.10.1
+-- Generation Time: Feb 02, 2014 at 06:11 PM
+-- Server version: 5.5.35-0ubuntu0.13.10.2
 -- PHP Version: 5.5.3-1ubuntu2.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -28,13 +28,12 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `allNotes` (
   `index` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `idno` int(10) unsigned NOT NULL,
+  `userid` varchar(20) COLLATE latin1_general_ci NOT NULL,
   `heading` varchar(500) COLLATE latin1_general_ci NOT NULL,
   `data` varchar(1000) COLLATE latin1_general_ci NOT NULL,
-  `done` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`index`),
   KEY `index` (`index`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=78 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=18 ;
 
 -- --------------------------------------------------------
 
@@ -43,13 +42,13 @@ CREATE TABLE IF NOT EXISTS `allNotes` (
 --
 
 CREATE TABLE IF NOT EXISTS `userdata` (
-  `idno` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` varchar(20) COLLATE latin1_general_ci NOT NULL,
   `name` varchar(50) COLLATE latin1_general_ci NOT NULL,
   `email` varchar(50) COLLATE latin1_general_ci NOT NULL,
-  `password` varchar(50) COLLATE latin1_general_ci NOT NULL,
-  PRIMARY KEY (`idno`),
-  UNIQUE KEY `idno` (`idno`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=15 ;
+  `password` varchar(100) COLLATE latin1_general_ci NOT NULL,
+  PRIMARY KEY (`userid`),
+  UNIQUE KEY `idno` (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
